1ee9c37249499de09d6d814327b120eb30ec9337
Dclasyc <Akarigbooladipo@gmail.com>
2023-11-24 04:46:10 +0100

    Integration Test for all GET Requests

--- src/test/java/com/dclasyc/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/dclasyc/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -7,10 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
-import java.awt.*;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
