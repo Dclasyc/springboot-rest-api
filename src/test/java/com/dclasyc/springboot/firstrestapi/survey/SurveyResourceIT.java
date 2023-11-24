e439f4f337112778c14938b0d712a18cf3b6aeca
Dclasyc <Akarigbooladipo@gmail.com>
2023-11-24 04:44:17 +0100

    Integration Test for all GET Requests

--- src/test/java/com/dclasyc/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/dclasyc/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -8,13 +8,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
+import java.awt.*;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyResourceIT {
 
+    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+    private static String GET_ALL_QUESTIONS_URL = "/surveys/Survey1/questions";
+    private static String GET_ALL_SURVEYS_URL = "/surveys";
+    private static String GET_SPECIFIC_SURVEY_URL = "/surveys/Survey1";
+    @Autowired
+    private TestRestTemplate template;
+
     String str = """
-            
+                        
             {
               "id": "Question1",
               "description": "Most Popular Cloud Platform Today",
@@ -26,15 +37,8 @@ public class SurveyResourceIT {
               ],
               "correctAnswer": "AWS"
             }
-            
+                        
             """;
-
-    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
-    @Autowired
-    private TestRestTemplate template;
-
-    //{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
-    //[Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Thu, 23 Nov 2023 01:20:13 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]
 
     @Test
     void retrieveSurveyQuestionById_BasicScenario() throws JSONException {
@@ -48,12 +52,104 @@ public class SurveyResourceIT {
 
                 }
                 """;
-//        assertEquals(expectedResponse.trim(), responseEntity.getBody());
-        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(),false);
 
-//        System.out.println(responseEntity.getBody());
-//        System.out.println(responseEntity.getHeaders());
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
 
+    }
+
+    @Test
+    void retrieveAllSurveyQuestions_BasicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(GET_ALL_QUESTIONS_URL, String.class);
+
+        // 20231124041428
+        // http://localhost:8080/surveys/Survey1/questions
+        String expectedResponse = """
+
+                  [
+                    {
+                      "id": "Question1"
+                    },
+                    {
+                      "id": "Question2"
+                      },
+                    {
+                      "id": "Question3"
+                      }
+                  ]
+                """;
+        //Check for Content-Type returned
+        //Check Status of request
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+
+    }
+
+    @Test
+    void retrieveAllSurvey_BasicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(GET_ALL_SURVEYS_URL, String.class);
+        // 20231124042856
+        // http://localhost:8080/surveys
+
+        String expectedResponse = """
+                  
+                                   [
+                                     {
+                                       "id": "Survey1",
+
+                                       "questions": [
+                                         {
+                                           "id": "Question1"
+                                         },
+                                         {
+                                           "id": "Question2"
+                                          },
+                                         {
+                                           "id": "Question3"
+                                           }
+                                       ]
+                                     }
+                                   ]
+                """;
+        //Check for Content-Type returned
+        //Check Status of request
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+    }
+
+    @Test
+    void getSurveyById_BasicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(GET_SPECIFIC_SURVEY_URL, String.class);
+// 20231124043550
+// http://localhost:8080/surveys/Survey1
+        String expectedResponse = """
+                  
+                                   
+                                     {
+                                       "id": "Survey1",
+
+                                       "questions": [
+                                         {
+                                           "id": "Question1"
+                                         },
+                                         {
+                                           "id": "Question2"
+                                          },
+                                         {
+                                           "id": "Question3"
+                                           }
+                                       ]
+                                     }
+                                   
+                """;
+        //Check for Content-Type returned
+        //Check Status of request
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
     }
 
 }
